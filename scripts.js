75fce7a80fce1551a0f79279f3b33d0d8caa13c5
asdfcok25 <hartaldi7@gmail.com>
2025-05-05 01:16:56 +0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,18 +1,22 @@
 document.addEventListener('DOMContentLoaded', function() {
   const btnPoopDL = document.getElementById('btnPoopDL');
   const btnYoutube = document.getElementById('btnYoutube');
-  const iframeContainer = document.getElementById('iframeContainer');
-
-  function loadIframe(url) {
-    iframeContainer.innerHTML = `<iframe src="${url}" allowfullscreen></iframe>`;
-    iframeContainer.style.display = 'block';
-  }
+  const dynamicIframe = document.getElementById('dynamicIframe');
+  const modalTitle = document.getElementById('modalTitle');
+  const iframeModal = new bootstrap.Modal(document.getElementById('iframeModal'));
 
   btnPoopDL.addEventListener('click', function() {
-    loadIframe('https://poopdl.dapuntaratya.com/');
+    dynamicIframe.src = 'https://poopdl.dapuntaratya.com/';
+    modalTitle.textContent = 'PoopDL Downloader';
   });
 
   btnYoutube.addEventListener('click', function() {
-    loadIframe('https://www.youtube.com/');
+    dynamicIframe.src = 'https://www.youtube.com/';
+    modalTitle.textContent = 'YouTube';
+  });
+
+  // Reset iframe when modal is closed
+  document.getElementById('iframeModal').addEventListener('hidden.bs.modal', function() {
+    dynamicIframe.src = '';
   });
 });
